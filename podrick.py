e2fa53b6e9084be0663c02d88dd15a33db64337f
kevthanewversi <murimiclerk@gmail.com>
2018-01-17 23:15:04 +0000

    bot almost functional, for now : i always ask myself, what did podrick do to those 3 chics

--- podrick.py
+++ podrick.py
@@ -4,9 +4,12 @@ import subprocess
 import random
 import datetime
 from dateutil import parser
+from dateutil.parser import parse
 
 #pre-requisites sendmail & imported libs
 #remember to test for run times in the code of say having the dates data come from a file or from a dict
+
+#check if sendmail/postfix is installed
 
 mydict = {"apples": "Jan 17 2018", "oranges": "July 10"}
 messages = ["On your special day, I wish you good luck. I hope this wonderful day will fill up your heart with joy and blessings. Have a fantastic birthday, celebrate the happiness on every day of your life. Happy Birthday!!","Happy Birth","Love","Mamama"]
@@ -18,13 +21,20 @@ for key,value in mydict.items():
   #code to check if today's date is someones' bday
   #reg expression to check if date is in required format
   #ValueError: time data '' does not match format '%b %d %Y'
-   print(isinstance(value, str)) #if (value.class !=
-   birthdate = datetime.datetime.strptime(value, '%b %d %Y')
+  #remove this if data is coming from a CSV
+   if type(value)!= str:
+      print(key+"'s birthday isn't a valid date")
+   #birthdate = datetime.datetime.strptime(value, '%b %d %Y')
+   birthdate = parse(value)
    month = birthdate.month
+   day = birthdate.day
    if (month == now.month): 
      print(key,value)
      print("It's your birthday month")
-random = random.choice(messages)
+     if day == now.day:
+        random = random.choice(messages)
+        sendmail()
+
 
 def sendemail():
    process = subprocess.Popen(("echo",random),stdout=subprocess.PIPE) 
