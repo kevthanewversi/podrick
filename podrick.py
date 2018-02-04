8736f7c82570709704e07fdf10f9c4f4ea6f6895
kevthanewversi <murimiclerk@gmail.com>
2018-02-04 18:10:31 +0000

    *script organised into fuctions *using os.file.isfile() to check for file now *cleaned out test data

--- podrick.py
+++ podrick.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#import emoji 
+#import emoji
 import subprocess
 import random
 import datetime
@@ -8,38 +8,75 @@ from dateutil.parser import parse
 
 #pre-requisites sendmail & imported libs
 #remember to test for run times in the code of say having the dates data come from a file or from a dict
+#game of thrones quote
 
 #check if sendmail/postfix is installed
 #should be run once a month and store all the birthdays for that month
 
-mydict = {"apples": "Jan 17 2018", "oranges": "July 10"}
-messages = ["On your special day, I wish you good luck. I hope this wonderful day will fill up your heart with joy and blessings. Have a fantastic birthday, celebrate the happiness on every day of your life. Happy Birthday!!","Happy Birth","Love","Mamama"]
+
+
+birthday_dates = {"apples": "Jan 17 2018", "oranges": "July 10"}
+messages = ["Test","Test","Test","Test"]
 
 #use cases to chcek for certain months and check for certain days that match with guys' birthdays
 now = datetime.datetime.now()
 
-for key,value in mydict.items():
-  #code to check if today's date is someones' bday
-  #reg expression to check if date is in required format
-  #ValueError: time data '' does not match format '%b %d %Y'
-  #remove this if data is coming from a CSV
-   if type(value)!= str:
-      print(key+"'s birthday isn't a valid date")
-   #birthdate = datetime.datetime.strptime(value, '%b %d %Y')
-   birthdate = parse(value)
-   month = birthdate.month
-   day = birthdate.day
-   if (month == now.month): 
-     print(key,value)
+#consumes csv file and saves it data to a dict
+def process_csv(csv_file):
+    fieldnames = ['name', 'date']
+    data = csv.DictReader(csv_file,fieldnames = fieldnames,dialect='excel')
+    for row in data:
+        birthday_dates.append(row[name])
+        birthday_dates.append(row[date])
+    validate_birthday_dates(birthday_dates)
+    return True
+
+#store values in dict infinitely
+# csv encoding -----check this <<<<<<<<<<
+
+'''
+Method that checks if the names and birthdays in the csv file
+are strings and aren't empty values 
+'''
+def validate_birthday_dates(birthday_dates):
+    #for key,value in birthday_dates.items():
+    count = 0
+    for row in birthday_dates.items():
+        #code to check if today's date is someones' bday
+    #reg expression to check if date is in required format
+    #ValueError: time data '' does not match format '%b %d %Y'
+    #remove this if data is coming from a CSV
+        if type(row[date])!= str | if type (row[name]) != str:
+             print(row[name]+"'s birthday isn't a valid date")
+        if not row[name] | not row ['data']:
+             print("Row %s has an empty/empty values. Enter a name 
+                     & birthday date or delete the row before export
+                     your excel file to csv format", (count))
+        count += count     
+     #birthdate = datetime.datetime.strptime(value, '%b %d %Y')
+     birthdate = parse(value)
+     month = birthdate.month
+     day = birthdate.day
+     if (month == now.month):
+         print(key,value)
      print("It's your birthday month")
      if day == now.day:
-        random = random.choice(messages)
-        sendmail()
+         random = random.choice(messages)
+         sendmail()
 
 
 def sendemail():
-   process = subprocess.Popen(("echo",random),stdout=subprocess.PIPE) 
+    process = subprocess.Popen(("echo",random),stdout=subprocess.PIPE)
    output  = subprocess.check_output(( "mail", "-s", "Birthday wishes", "test@gmail.com"), stdin=process.stdout)
    (out, err) = process.communicate()
 
-sendemail()
+if __name__ == "__main__":
+
+    #handle this else
+   if os.file.isfile("birthdays.csv"):
+       csv_file = open("birthdays.csv","r")
+       process_csv(csv_file)
+   #else:
+   #    if not birthday_dates:
+   #        print ("Enter some ")
+   #sendemail()
